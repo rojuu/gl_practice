8d44d3d77e11dbee2a22fd57fb3c0d826ab19b00
Roni Juppi <roni.juppi@gmail.com>
2017-07-16 18:33:22 +0300

    Drawing a simple triangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,13 @@
 
 #include "util.h"
 
+// An array of 3 vectors which represents 3 vertices
+static const GLfloat g_vertex_buffer_data[] = {
+   -1.0f, -1.0f, 0.0f,
+   1.0f, -1.0f, 0.0f,
+   0.0f,  1.0f, 0.0f,
+};
+
 int main(int argc, char **argv) {
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
         println("SDL_Error: %c\n", SDL_GetError());
@@ -16,8 +23,7 @@ int main(int argc, char **argv) {
         "GL_TEST", 
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        512,
-        512,
+        1024, 768,
         SDL_WINDOW_OPENGL
     );
 
@@ -32,66 +38,81 @@ int main(int argc, char **argv) {
         SDL_GL_CONTEXT_PROFILE_CORE
     );
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     SDL_GL_SetSwapInterval(1);
 
-    #ifndef __APPLE__
-	glewExperimental = GL_TRUE;
-	glewInit();
-	#endif
+#if 0
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+#endif
 
+    #ifndef __APPLE__
+    glewExperimental = GL_TRUE;
+    glewInit();
+    #endif
+
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
     glClearColor(0.0, 0.0, 0.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	SDL_GL_SwapWindow(window);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(window);
 
     bool running = true;
     while (running)
-	{
-		SDL_Event event;
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_QUIT){
-				running = false;
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT){
+                running = false;
             }
 
-			if (event.type == SDL_KEYDOWN)
-			{
-				switch (event.key.keysym.sym)
-				{
-				case SDLK_ESCAPE:
-					running = false;
-					break;
-				case SDLK_r:
-					glClearColor(1.0, 0.0, 0.0, 1.0);
-					glClear(GL_COLOR_BUFFER_BIT);
-					SDL_GL_SwapWindow(window);
-					break;
-				case SDLK_g:
-					glClearColor(0.0, 1.0, 0.0, 1.0);
-					glClear(GL_COLOR_BUFFER_BIT);
-					SDL_GL_SwapWindow(window);
-					break;
-				case SDLK_b:
-					glClearColor(0.0, 0.0, 1.0, 1.0);
-					glClear(GL_COLOR_BUFFER_BIT);
-					SDL_GL_SwapWindow(window);
-					break;
-				default:
-					break;
-				}
-			}
-		}
-	}
+            if (event.type == SDL_KEYDOWN)
+            {
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_ESCAPE:
+                    running = false;
+                    break;                 
+                default:
+                    break;
+                }
+            }
+        }
 
-	SDL_GL_DeleteContext(context);
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
+        SDL_GL_SwapWindow(window);
+    }
 
-	SDL_DestroyWindow(window);
+    SDL_GL_DeleteContext(context);
 
-	SDL_Quit();
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
     return 0;
 }
